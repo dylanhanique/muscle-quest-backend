2126c973cabb39c8763001c25a9d0e7b71389601
Dylan Hanique <dylan.hanique@proton.me>
2025-08-11 02:46:39 +0200

    refactor(exception-filter): add message property, and default status
    Add 'message' property, and set status to INTERNAL_SERVER_ERROR when
     the exception is not an instance of HttpException.

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -4,6 +4,7 @@ import {
   ArgumentsHost,
   HttpException,
   Logger,
+  HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
@@ -14,13 +15,23 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
 
-    this.logger.error(' ⚠ EXCEPTION ⚠ ', exception.getResponse());
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : '';
+
+    this.logger.error(
+      `Error on ${request.method} ${request.url}: ${JSON.stringify(message)}`,
+    );
 
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
+      message,
       path: request.url,
     });
   }
