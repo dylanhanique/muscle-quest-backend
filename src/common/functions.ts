363c4c9a076b28786524f37b3204cc27428795dc
Dylan Hanique <dylan.hanique@proton.me>
2025-08-14 04:39:58 +0200

    refactor(auth): move hashToken to auth service; add readonly to constructor services

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,19 +1,7 @@
-import { createHash, randomBytes } from 'crypto';
-
 export function getEnvVar(name: string): string {
   const value = process.env[name];
   if (!value) {
     throw new Error(`Missing env var: ${name}`);
   }
   return value;
-}
-
-export function hashToken(token: string) {
-  const salt = randomBytes(16).toString('hex');
-
-  const hash = createHash('sha256')
-    .update(salt + token)
-    .digest('hex');
-
-  return { hash, salt };
 }
